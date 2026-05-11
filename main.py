fbee7173e3541416979e37de3564fd71e6b4358b
eag1e0w1 <alexeyafilin@gmail.com>
2026-05-11 19:24:15 +0300

    main.py: добавлена конструкция if __name__ ...

--- main.py
+++ main.py
@@ -5,35 +5,37 @@ from config import PATH
 from simulator import Warehouse
 from calculator import Calculator
 
-sim = Warehouse()
-calc = Calculator()
-params = load_warehouse_params(PATH)
 
-scenarios = {
-    'no_amr': params.model_copy(update={'amr_num': 0,
-                                     'productivity': 1,
-                                     'pickers': 40}),
-    'amr': params.model_copy(update={'amr_num': 100,
-                                     'productivity': 1.4,
-                                     'pickers': 26})
-}
+if __name__ == '__main__':
+    sim = Warehouse()
+    calc = Calculator()
+    params = load_warehouse_params(PATH)
 
-metrics = {}
-for name, param in scenarios.items():
-    data = sim.run(param)
-    result = calc.calculate(param, data)
-    metrics[name] = {'params': param, 'result': result, 'data': data}
+    scenarios = {
+        'no_amr': params.model_copy(update={'amr_num': 0,
+                                         'productivity': 1,
+                                         'pickers': 40}),
+        'amr': params.model_copy(update={'amr_num': 100,
+                                         'productivity': 1.4,
+                                         'pickers': 26})
+    }
+
+    metrics = {}
+    for name, param in scenarios.items():
+        data = sim.run(param)
+        result = calc.calculate(param, data)
+        metrics[name] = {'params': param, 'result': result, 'data': data}
 
 
-df = pd.DataFrame(index=[key for key in metrics['no_amr']['result'].keys()],
-                 data={'no_amr':[value for value in metrics['no_amr']['result'].values()],
-                       'amr':[value for value in metrics['amr']['result'].values()]})
+    df = pd.DataFrame(index=[key for key in metrics['no_amr']['result'].keys()],
+                     data={'no_amr':[value for value in metrics['no_amr']['result'].values()],
+                           'amr':[value for value in metrics['amr']['result'].values()]})
 
-for col in df.columns:
-    df[col] = round(df[col],1)
+    for col in df.columns:
+        df[col] = round(df[col],1)
 
-df['effect'] = df['amr'] - df['no_amr']
-min_monthly_profit = ceil(calc.compute_min_monthly_profit(df))
+    df['effect'] = df['amr'] - df['no_amr']
+    min_monthly_profit = ceil(calc.compute_min_monthly_profit(df))
 
-print(df)
-print(min_monthly_profit)
+    print(df)
+    print(min_monthly_profit)
